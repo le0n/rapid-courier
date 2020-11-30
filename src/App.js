6aa64067010a76fc77e0ac0b452843cdc4f7d081
Leon Zheng <leon.zheng@hotmail.com>
2020-11-30 13:54:44 +1000

    initiated react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          this is a simple website
         </p>
         <a
           className="App-link"
