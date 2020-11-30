6dfb11e9c97630e113d9b0910b9712ae3a8a0e09
Leon Zheng <leon.zheng@hotmail.com>
2020-12-01 00:18:55 +1000

    seperated the components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import React, {component} from 'react';
+import React from 'react';
+import { Router } from "@Reach/router";
 
-firebase.initializeApp({
+const config = {
   apiKey: "AIzaSyCXqvu1j3NQ4J303SKtBAcXZ_iFz5LhUdc",
   authDomain: "rapid-87eab.firebaseapp.com",
   databaseURL: "https://rapid-87eab.firebaseio.com",
@@ -13,7 +14,11 @@ firebase.initializeApp({
   messagingSenderId: "724929581922",
   appId: "1:724929581922:web:5fa2ed8c80b8e52498d23b",
   measurementId: "G-SN0R0SLVTZ"
-})
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -22,9 +27,19 @@ const firestore = firebase.firestore();
 function App() {
   return (
     <div className="App">
-      <h1>hi i'm a react app</h1>
+      <h1>hi i'm a react app11</h1>
+      <Title/>
     </div>
+    
   )
 }
 
 export default App;
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1> {">"} Rapid Courier</h1>
+    </div>
+  )
+}
---
+++ src/Components/Login.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+
+const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+}
