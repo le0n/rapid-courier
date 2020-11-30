9cf09decc1c9e0a757421cc5961b914e8e352474
Leon Zheng <leon.zheng@hotmail.com>
2020-11-30 14:23:49 +1000

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCXqvu1j3NQ4J303SKtBAcXZ_iFz5LhUdc",
+  authDomain: "rapid-87eab.firebaseapp.com",
+  databaseURL: "https://rapid-87eab.firebaseio.com",
+  projectId: "rapid-87eab",
+  storageBucket: "rapid-87eab.appspot.com",
+  messagingSenderId: "724929581922",
+  appId: "1:724929581922:web:5fa2ed8c80b8e52498d23b",
+  measurementId: "G-SN0R0SLVTZ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is a simple website!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
