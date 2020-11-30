10b80ff93165c3a8d952e885e5c5403d3f886c23
Leon Zheng <leon.zheng@hotmail.com>
2020-11-30 14:32:12 +1000

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -25,11 +25,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
+        <h1>this is a test website</h1>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
