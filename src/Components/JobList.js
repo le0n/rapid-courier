2950be8ad52b82a055f5c4a24b5f54ffb3efeef6
Leon Zheng <leon.zheng@hotmail.com>
2020-12-02 19:14:52 +1000

    set up async

--- src/Components/JobList.js
+++ src/Components/JobList.js
@@ -1,32 +1,39 @@
-import React, {useState} from "react";
+import React, {Component, useState, useEffect} from "react";
 import Jobs from './Jobs.js';
 import '../index.css';
 import firebase from 'firebase/app';
 
-var jobArray = [];
-
-let fetchData = () => {
-    firebase.firestore().collection('jobs').get().then((querySnapshot) => { //this is an async method hence then()
-        querySnapshot.docs.forEach(doc => { //querySnapshot gives all data
-            //console.log(doc.data()); //doc.data() for actual data and doc.id for document id (for deletion)
-            var job = {'pickUp':doc.data().pickUp, 'dropOff':doc.data().dropOff, 'pay':doc.data().pay};
-            jobArray.push(job);
-        })
-    })
-}
 
 //https://firebase.google.com/docs/firestore/query-data/get-data#web_5
-const JobList = () => {
-    fetchData();
-    return (
-        <div>
-            <Jobs pickUp='test1' dropOff='test2' pay='12.40' />
-        </div>
-      
 
+const fetchData = async () => {
+    var collectionRef = firebase.firestore().collection('jobs');
+    const  querySnapshot = await collectionRef.get();
+    return querySnapshot.docs.map(doc => [doc.id, doc.data()]); //returns a promise
+}
+const JobList = () => {
+    const [jobArray, setJobArray] = useState([]);
+    
+    const executeFetchData = async () => {
+        let data = await fetchData();
+        console.log(data);
+    }
+    
+
+    //console.log(executeFetchData());
+    //fetchData().then((data) => console.log(data));
+
+    useEffect(() => {
+        //fetchData().then((data) => console.log(data));
+        //setJobArray(['0',{'pickup':'111 st Oxleyu'}]);
+        console.log(executeFetchData());
+    });
+    return (
+        <ul>
+            {jobArray.map(job => (<li key={jobArray[0]}>{jobArray[1].pickUp}</li>))}
+        </ul>
     )
 }
-
 
 
 export default JobList;
